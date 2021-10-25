091ea05f095ba30dd656ac59c6dd5dc425341901
Alan Tavares <alan.am.tavares@gmail.com>
2021-10-25 16:05:51 -0300

    Atualizado - Envio de mensagem via fila pusher

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,10 @@ Route::get('/', function () {
 
 Route::get('/event', function () {
 //    MessageNotification::dispatch('This is the first message');
-    event(new MessageNotification('This is the first message'));
+    $data['nome'] = 'alan';
+    $data['url'] = 'http://192.168.88.231/images/0D0j22w1wh33L58b2j21185Vc4b1tIJ1Ry.jpg';
+    event(new MessageNotification($data));
+    echo 'message send';
 });
 
 Route::get('/listen', function () {
