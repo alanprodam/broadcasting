fc42e7b7f448ce4acbea31f251613f9c5952f2e2
Alan Tavares <alan.am.tavares@gmail.com>
2021-10-25 12:52:18 -0300

    first commit

---
+++ resources/views/listen.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA_Compatible" content="ie=edge">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>Event</title>
+
+    </head>
+    <body>
+        <div id="app">
+            Ola!
+        </div>
+        <script src="{{ asset('js/app.js')  }}"></script>
+    </body>
+</html>
